0be809cf214552cff8686909e9138b9bd0f1cbfc
Alexis Magana <amalexism49@gmail.com>
2018-10-05 03:30:12 -0400

    Fixed code

--- shellscripthw.sh
+++ shellscripthw.sh
@@ -1,2 +1,8 @@
 #usage bash shellscripthw.sh
-cat wages.csv | cut -d , -f 1,2 | grep -v 'gender' |  sort -u | sed 's/,/ /g' > unique_gender_yearsexperience.txt
+
+echo "Highest earner"
+cat wages.csv|sed 's/,/ /g'|sort -k 4n|sed 's/ /,/g'|tail -n 1;
+echo "Lowest earner"
+cat wages.csv|sed 's/,/ /g'|sort -k 4n|sed 's/ /,/g'|grep -v 'wage'|head -n 1;
+echo "Number of females in top ten earners"
+cat wages.csv|sed 's/,/ /g'|sort -k 4n|sed 's/ /,/g'|tail -n 10|grep 'female'|wc -l;
