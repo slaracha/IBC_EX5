c6ff38e411c81982c93713546a054d6337189dcf
Saury Lara Chalas <saurylara@Saurys-MBP.mshome.net>
2018-10-05 03:28:10 -0400

    Created a script to sort

--- shellscripthw.sh
+++ shellscripthw.sh
@@ -1,2 +1,2 @@
 #usage bash shellscripthw.sh
-cat wages.csv | cut -d , -f 1,2 | grep -v 'gender' |  sort -u | sed 's/,/ /g' > unique_gender_yearsexperience.txt
+cat wages.csv | cut -d , -f 1,2 | grep -v 'gender' |  sort -u | sed 's/,/ /g' | sort -k1,1 -k2n 
